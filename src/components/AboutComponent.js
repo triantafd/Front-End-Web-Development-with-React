ec060e6ff99c42b025a9a9f0ab291db9e331e543
dimitris <triantafd@iti.gr>
2022-02-08 20:07:20 +0200

    Fix error (unique key missing) in AboutusPage

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function RenderLeader({ leader }) {
   return (
-    <div key={leader.id} className="col-12 mt-5">
+    <div className="col-12 mt-5">
       <Media tag="li">
         <Media left middle>
           <Media object src={leader.image} alt={leader.name} />
@@ -23,7 +23,7 @@ function About(props) {
 
   const leaders = props.leaders.map((leader) => {
     return (
-      <RenderLeader leader={leader} />
+      <RenderLeader leader={leader} key={leader.id} />
     );
   });
 
