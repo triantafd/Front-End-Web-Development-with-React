6b87b484bd6ca56e672aab8df8ecbe9b6499590e
dimitris <triantafd@iti.gr>
2022-02-09 22:07:55 +0200

    Redux Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
@@ -10,5 +12,6 @@ export const ConfigureStore = createStore(
     comments: Comments,
     promotions: Promotions,
     leaders: Leaders
-  })
+  }),
+  applyMiddleware(thunk, logger)
 );
